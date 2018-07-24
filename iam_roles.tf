39ea5561b29dcce3ab60d60ad0671fea604e5c07
Bryan Shelton <bryan@rover.com>
2018-07-24 11:15:30 -0700

    Distinct worker role and taints

--- iam_roles.tf
+++ iam_roles.tf
@@ -69,3 +69,44 @@ resource "aws_iam_role_policy_attachment" "workers_AmazonEC2ContainerRegistryRea
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = "${aws_iam_role.workers.name}"
 }
+
+# Kiam Workers
+resource "aws_iam_role" "kiam_workers" {
+  name_prefix = "${aws_eks_cluster.this.name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "kiam_workers" {
+  name_prefix = "${aws_eks_cluster.this.name}"
+  role        = "${aws_iam_role.kiam_workers.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "kiam_workers_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = "${aws_iam_role.kiam_workers.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "kiam_workers_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = "${aws_iam_role.kiam_workers.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "kiam_workers_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = "${aws_iam_role.kiam_workers.name}"
+}
--- workers.tf
+++ workers.tf
@@ -2,11 +2,29 @@ module "kiam_workers" {
   source = "./worker_group"
 
   cluster_name                = "${var.cluster_name}"
-  worker_group_name           = "workers"
+  worker_group_name           = "kiam"
   subnets                     = ["${module.vpc.public_subnets}"]
   associate_public_ip_address = true
   security_groups             = ["${aws_security_group.workers.id}"]
   cluster_auth_base64         = "${aws_eks_cluster.this.certificate_authority.0.data}"
   cluster_endpoint            = "${aws_eks_cluster.this.endpoint}"
   iam_instance_profile_id     = "${aws_iam_instance_profile.workers.id}"
+
+  kubelet_node_labels          = "node-role.kubernetes.io/kiam="
+  kubelet_register_with_taints = "node-role.kubernetes.io/master=:NoSchedule"
+}
+
+module "workers" {
+  source = "./worker_group"
+
+  cluster_name                = "${var.cluster_name}"
+  worker_group_name           = "workers"
+  subnets                     = ["${module.vpc.public_subnets}"]
+  associate_public_ip_address = true
+  security_groups             = ["${aws_security_group.workers.id}"]
+  cluster_auth_base64         = "${aws_eks_cluster.this.certificate_authority.0.data}"
+  cluster_endpoint            = "${aws_eks_cluster.this.endpoint}"
+  iam_instance_profile_id     = "${aws_iam_instance_profile.kiam_workers.id}"
+
+  kubelet_node_labels = "node-role.kubernetes.io/worker="
 }
